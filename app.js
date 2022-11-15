baf30f9b82f6e587c34af40ee94578004215b180
Marselinus Setyawan <marselinusphs@gmail.com>
2022-11-15 12:46:47 +0800

    fix

--- app.js
+++ app.js
@@ -3,13 +3,10 @@ const app = express();
 const allRoutes = require("./routes");
 const dotenv = require("dotenv");
 
-// get config vars
 dotenv.config();
-
-// access config var
 process.env.TOKEN_SECRET;
 
-const PORT = 3500;
+const PORT = 3200;
 
 app.use(express.json());
 app.use(allRoutes);
